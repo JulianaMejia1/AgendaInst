3e9aeb057494cee02bf00390c7fdf51d547f473c
JulianaMejia1 <162184490+JulianaMejia1@users.noreply.github.com>
2024-10-02 23:23:21 -0500

    a

---
+++ Php/Conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="mydb";
+/*
+try{
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8",$username,$password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	echo "Conexión con el servidor establecida correctamente";
+}catch(PDOException $e){
+	echo "Conexión con el servidor fallida".$e->getMessage();
+}*/
+?>
